62e2a6db2e1c8e27fddfdfe76b8efe08a2e6e94e
cybanjar <cybanjar@gmail.com>
2020-10-01 10:36:09 +0700

    add button images ota

--- src/pages/Ota.vue
+++ src/pages/Ota.vue
@@ -4,21 +4,28 @@
       <h4>Cari Reservasi Anda</h4>
       <p>Cari dengan memilih salah satu opsi dibawah</p>
 
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+      <div class="q-pa-md">
+        <div class="row justify-center">
+          <div class="col-12 col-md-auto">
+            <img src="../assets/booking-code.svg" />
+          </div>
+          <div class="col-12 col-md-auto">
+            <img src="../assets/Nama.svg" />
+          </div>
+          <div class="col-12 col-md-auto">
+            <img src="../assets/EmailAddress.svg" />
+          </div>
+          <div class="col-12 col-md-auto">
+            <img src="../assets/membership.svg" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Error404",
+  name: "Test",
 };
 </script>
