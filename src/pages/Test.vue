7b23043b0d73672b52071f48a29a909b95f49eed
cybanjar <cybanjar@gmail.com>
2020-10-01 08:56:09 +0700

    Testing Routing

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
     <div>
-      <div style="font-size: 30vh">404</div>
+      <div style="font-size: 30vh">Achooo!</div>
 
-      <div class="text-h2" style="opacity:.4">Oops. Nothing here...</div>
+      <div class="text-h2" style="opacity:.4">Testing Routing...</div>
 
       <q-btn
         class="q-mt-xl"
