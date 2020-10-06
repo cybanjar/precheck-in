6daab5657617b98ff82c4097e2b44d2de545159b
parys410 <parys410@gmail.com>
2020-10-06 21:55:37 +0800

    Add custom height style to _form.scss, Add H1,H2 style to _text.scss, Add class ant-input-h to About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,9 +6,16 @@
   </div>
   <div v-else>
     <div class="home">
-      <a-row class="header-brandings" :style="information" type="flex" justify="space-between">
+      <a-row
+        class="header-brandings"
+        :style="information"
+        type="flex"
+        justify="space-between"
+      >
         <a-col class="pl-3 pt-3" :span="15" :md="15" :xs="24">
-          <h4 class="mb-3 text-white font-weight-bold" :style="information">ONLINE CHECK-IN</h4>
+          <h4 class="mb-3 text-white font-weight-bold" :style="information">
+            ONLINE CHECK-IN
+          </h4>
           <h6
             v-if="currDataPrepare['guest-member-name'] !== ''"
             class="main-guest-title text-white font-weight-bold"
@@ -18,7 +25,11 @@
             {{ currDataPrepare["guest-pname"] }} |
             {{ currDataPrepare["guest-member-name"] }}
           </h6>
-          <h6 v-else class="main-guest-title text-white font-weight-bold" :style="information">
+          <h6
+            v-else
+            class="main-guest-title text-white font-weight-bold"
+            :style="information"
+          >
             {{ currDataPrepare["guest-lname"] }},
             {{ currDataPrepare["guest-pname"] }}
           </h6>
@@ -34,7 +45,11 @@
           </p>
         </a-col>
         <a-col class="container" :span="9" :md="9" :xs="24">
-          <img class="img-hotel float-right image" :src="gambar" alt="Image Loading" />
+          <img
+            class="img-hotel float-right image"
+            :src="gambar"
+            alt="Image Loading"
+          />
           <div class="overlay">
             <div class="text">{{ hotelname }}</div>
           </div>
@@ -170,7 +185,12 @@
                       >
                         <q-time v-model="hour" mask="HH:mm" format24h>
                           <div class="row items-center justify-end">
-                            <q-btn v-close-popup label="Close" color="primary" flat />
+                            <q-btn
+                              v-close-popup
+                              label="Close"
+                              color="primary"
+                              flat
+                            />
                           </div>
                         </q-time>
                       </q-popup-proxy>
@@ -179,23 +199,38 @@
                 </q-input>
               </a-form-item>
             </a-col>
-            <a-col :span="4" :xl="6" :lg="5" :md="6" :xs="24" v-show="showPickupRequest">
+            <a-col
+              :span="4"
+              :xl="6"
+              :lg="5"
+              :md="6"
+              :xs="24"
+              v-show="showPickupRequest"
+            >
               <a-form-item :label="getLabels('request')">
                 <a-checkbox
                   :checked="showPrice"
                   v-model="showPrice"
                   @change="onChange"
-                >{{ getLabels("pick_req") }}</a-checkbox>
+                  >{{ getLabels("pick_req") }}</a-checkbox
+                >
               </a-form-item>
             </a-col>
-            <a-col :span="4" :xl="4" :lg="5" :md="5" :xs="24" v-show="showPickupRequest">
+            <a-col
+              :span="4"
+              :xl="4"
+              :lg="5"
+              :md="5"
+              :xs="24"
+              v-show="showPickupRequest"
+            >
               <a-form-item :label="getLabels('price')">
                 <label v-decorator="['currency', { initialValue: money }]">
                   {{ nilai === 3 ? "" : currency }}
                   {{
-                  nilai === 3
-                  ? " "
-                  : `${money}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " "
+                    nilai === 3
+                      ? " "
+                      : `${money}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " "
                   }}
                 </label>
                 <span>/ {{ per }}</span>
@@ -211,6 +246,7 @@
             >
               <a-form-item :label="getLabels('pick_detail')">
                 <a-input
+                  class="ant-input-h"
                   v-decorator="[
                     'flight',
                     {
@@ -224,37 +260,37 @@
           <a-row class="ml-3" gutter="16">
             <a-col>
               <a-form-item :label="getLabels('room_pref')">
-                <a-radio-group name="radioGroup" v-show="showSmoking" @change="Room">
+                <a-radio-group
+                  name="radioGroup"
+                  v-show="showSmoking"
+                  @change="Room"
+                >
                   <a-radio value="NonSmoking">
                     <span class="font-weight-normal">
-                      {{
-                      getLabels("non_smoking")
-                      }}
+                      {{ getLabels("non_smoking") }}
                     </span>
                   </a-radio>
                   <a-radio value="Smoking">
                     <span class="font-weight-normal">
-                      {{
-                      getLabels("smoking")
-                      }}
+                      {{ getLabels("smoking") }}
                     </span>
                   </a-radio>
                 </a-radio-group>
               </a-form-item>
               <a-form-item label>
-                <a-radio-group name="radioGroup" v-show="showFloor" @change="Floor">
+                <a-radio-group
+                  name="radioGroup"
+                  v-show="showFloor"
+                  @change="Floor"
+                >
                   <a-radio value="LowerFloor">
                     <span class="font-weight-normal">
-                      {{
-                      getLabels("lower_floor")
-                      }}
+                      {{ getLabels("lower_floor") }}
                     </span>
                   </a-radio>
                   <a-radio value="HigherFloor">
                     <span class="font-weight-normal">
-                      {{
-                      getLabels("higher_floor")
-                      }}
+                      {{ getLabels("higher_floor") }}
                     </span>
                   </a-radio>
                 </a-radio-group>
@@ -263,16 +299,12 @@
                 <a-radio-group name="radioGroup" v-show="showBed" @change="Bed">
                   <a-radio value="OneBigBed">
                     <span class="font-weight-normal">
-                      {{
-                      getLabels("one_big_bed")
-                      }}
+                      {{ getLabels("one_big_bed") }}
                     </span>
                   </a-radio>
                   <a-radio value="TwoSingleBeds">
                     <span class="font-weight-normal">
-                      {{
-                      getLabels("two_single_beds")
-                      }}
+                      {{ getLabels("two_single_beds") }}
                     </span>
                   </a-radio>
                 </a-radio-group>
@@ -300,9 +332,17 @@
             </a-card>
           </a-row>
           <a-row class="ml-3" :gutter="[16, 8]">
-            <a-col v-if="email != ''" :span="5" :xl="5" :lg="7" :md="10" :xs="24">
+            <a-col
+              v-if="email != ''"
+              :span="5"
+              :xl="5"
+              :lg="7"
+              :md="10"
+              :xs="24"
+            >
               <a-form-item :label="getLabels('email')">
                 <a-input
+                  class="ant-input-h"
                   v-decorator="[
                     'email',
                     {
@@ -318,6 +358,7 @@
               <span>{{ currDataPrepare["guest-email"] }}</span>
               <a-form-item :label="getLabels('email')">
                 <a-input
+                  class="ant-input-h"
                   v-decorator="[
                     'email',
                     {
@@ -340,6 +381,7 @@
             <a-col :span="5" :xl="5" :lg="7" :md="10" :xs="24">
               <a-form-item :label="getLabels('phone_number')">
                 <a-input
+                  class="ant-input-h"
                   v-decorator="[
                     'phone',
                     {
@@ -367,7 +409,8 @@
                     v-for="item in FilterPurposeofStay"
                     :key="item"
                     :value="item.setupvalue"
-                  >{{ item.setupvalue }}</a-select-option>
+                    >{{ item.setupvalue }}</a-select-option
+                  >
                 </a-select>
               </a-form-item>
             </a-col>
@@ -389,7 +432,8 @@
                     v-for="item in FilterCountry"
                     :key="item"
                     :value="item['descr']"
-                  >{{ item.setupvalue }}</a-select-option>
+                    >{{ item.setupvalue }}</a-select-option
+                  >
                 </a-select>
                 <!-- <a-select-option value="Indonesia">Indonesia</a-select-option>
                   <a-select-option value="America">America</a-select-option>
@@ -427,7 +471,8 @@
                     v-for="item in FilterCountry"
                     :key="item"
                     :value="item['descr']"
-                  >{{ item.setupvalue }}</a-select-option>
+                    >{{ item.setupvalue }}</a-select-option
+                  >
                 </a-select>
               </a-form-item>
             </a-col>
@@ -448,13 +493,15 @@
                       v-for="item in filteredProvince"
                       :key="item"
                       :value="item['descr']"
-                    >{{ item.setupvalue }}</a-select-option>
+                      >{{ item.setupvalue }}</a-select-option
+                    >
                   </a-select>
                 </a-form-item>
               </div>
               <div v-else>
                 <a-form-item :label="getLabels('state')">
                   <a-input
+                    class="ant-input-h"
                     v-decorator="[
                       'State',
                       {
@@ -489,7 +536,8 @@
                   type="primary"
                   :loading="loading"
                   @click="handleOkTerm"
-                >{{ getLabels("close") }}</a-button>
+                  >{{ getLabels("close") }}</a-button
+                >
               </template>
               <p>{{ term }}</p>
             </a-modal>
@@ -505,7 +553,8 @@
                   :size="size"
                   :disabled="!agree"
                   html-type="submit"
-                >{{ getLabels("ci_now") }}</a-button>
+                  >{{ getLabels("ci_now") }}</a-button
+                >
               </a-form-item>
             </a-col>
           </a-row>
