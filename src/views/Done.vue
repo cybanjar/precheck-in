ecf2a93d755ba7aa8ed5766f2460b142cfd9c9cc
randitorizkysindata <r.andito@yahoo.com>
2020-10-09 13:20:09 +0700

    no message

--- src/views/Done.vue
+++ src/views/Done.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-result title="You already check in">
+  <a-result :title="getLabels('already_ci', `titleCase`)">
     <template #icon>
       <a-icon type="smile" theme="twoTone" />
     </template>
@@ -14,6 +14,36 @@
 export default {
   data() {
     return {};
+  },
+  methods: {
+    getLabels(nameKey, used) {
+      const label = this.labels.find(
+        (element) => element["program-variable"] == nameKey
+      );
+
+      let fixLabel = "";
+
+      if (label["program-label1"] == "undefined") {
+        fixLabel = "";
+      } else {
+        if (used === "titleCase") {
+          fixLabel = this.setTitleCase(label["program-label1"]);
+        } else if (used === "sentenceCase") {
+          fixLabel =
+            label["program-label1"].charAt(0).toUpperCase() +
+            label["program-label1"].slice(1);
+        } else {
+          fixLabel = label["program-label1"];
+        }
+      }
+
+      return fixLabel;
+    },
+    setTitleCase(label) {
+      return label.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      });
+    },
   },
 };
 </script>
