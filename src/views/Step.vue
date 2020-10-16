16d3f852372e57b7eba4d406ea920718c57641d9
randitorizkysindata <r.andito@yahoo.com>
2020-10-16 13:03:13 +0700

    no message

--- src/views/Step.vue
+++ src/views/Step.vue
@@ -15,14 +15,14 @@
       >
         <template slot="footer">
           <a-button key="back" @click="disagree">
-            {{ getLabels("disagree") }}
+            {{ getLabels("disagree",'titleCase') }}
           </a-button>
           <a-button
             key="submit"
             type="primary"
             :loading="loading"
             @click="handleOk"
-            >{{ getLabels("agree") }}</a-button
+            >{{ getLabels("agree",'titleCase') }}</a-button
           >
         </template>
         <p>{{ terms }}</p>
